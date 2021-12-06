d66dcb7a39d72d33f24686e0d4152f26b7d2488f
adamstory <adamjstory@gmail.com>
2021-12-06 16:50:37 +0000

    Task 2c Complete

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ let myCollection = [
   console.log(myCollection);
 
 
-  function describeItem(name) {
-    console.log(`I have a ${name}`);
-    console.log
+  function describeItem(item) {
+    console.log(item)
+    if (item.count === 1){
+    console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
+    }
+    if (item.count > 1){
+    console.log(`I have ${item.count}${item.name}s. Here's what I like about them: ${item.whatILike}`)
+    }
+  }
+
+  describeItem(myCollection[0]);
+
+  function describeCollection(array) {
+    describeItem();
   }
